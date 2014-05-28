d783bc487f9395606415c51871f59f6f29ba33ca
Chad Duberry <uwi.chadduberry@gmail.com>
2014-05-28 16:44:28 -0600

    Created Products class

---
+++ warehouse/app/controllers/Products.java
@@ -0,0 +1,32 @@
+package controllers;
+
+/**
+ * Created by CHAD on 5/28/14.
+ */
+
+import play.mvc.Controller;
+import play.mvc.Result;
+
+public class Products extends Controller
+{
+   public static Result list()
+   {
+      return TODO;
+   }
+
+   public static Result newProduct()
+   {
+      return TODO;
+   }
+
+   public static Result details( String ean )
+   {
+      return TODO;
+   }
+
+
+   public static Result save()
+   {
+      return TODO;
+   }
+}
