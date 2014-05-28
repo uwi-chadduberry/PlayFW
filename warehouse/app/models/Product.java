4df62a3f6fedeed169c34187159be1f610d87a61
Chad Duberry <uwi.chadduberry@gmail.com>
2014-05-28 17:26:07 -0600

    Adjusted product list location

--- warehouse/app/models/Product.java
+++ warehouse/app/models/Product.java
@@ -13,15 +13,6 @@ public class Product
    public String name;
    public String description;
    private static List<Product> products;
-   static
-   {
-      products = new ArrayList<Product>();
-      products.add( new Product( "1111111111111", "Paperclips 1", "Paperclips description 1" ) );
-      products.add( new Product( "2222222222222", "Paperclips 2", "Paperclips description 2" ) );
-      products.add( new Product( "3333333333333", "Paperclips 3", "Paperclips description 3" ) );
-      products.add( new Product( "4444444444444", "Paperclips 4", "Paperclips description 4" ) );
-      products.add( new Product( "5555555555555", "Paperclips 5", "Paperclips description 5" ) );
-   }
 
    public Product()
    {
@@ -33,6 +24,16 @@ public class Product
       this.ean = ean;
       this.name = name;
       this.description = description;
+   }
+
+   static
+   {
+      products = new ArrayList<Product>();
+      products.add( new Product( "1111111111111", "Paperclips 1", "Paperclips description 1" ) );
+      products.add( new Product( "2222222222222", "Paperclips 2", "Paperclips description 2" ) );
+      products.add( new Product( "3333333333333", "Paperclips 3", "Paperclips description 3" ) );
+      products.add( new Product( "4444444444444", "Paperclips 4", "Paperclips description 4" ) );
+      products.add( new Product( "5555555555555", "Paperclips 5", "Paperclips description 5" ) );
    }
 
    public static List<Product> findAll()
