00138cdea7c5fcc01efcb4887394343bf8c5f58c
Chad Duberry <uwi.chadduberry@gmail.com>
2014-05-28 17:09:55 -0600

    Implemented products and access methods

--- warehouse/app/models/Product.java
+++ warehouse/app/models/Product.java
@@ -3,11 +3,25 @@ package models;
 /**
  * Created by CHAD on 5/28/14.
  */
+
+import java.util.List;
+import java.util.ArrayList;
+
 public class Product
 {
    public String ean;
    public String name;
    public String description;
+   private static List<Product> products;
+   static
+   {
+      products = new ArrayList<Product>();
+      products.add( new Product( "1111111111111", "Paperclips 1", "Paperclips description 1" ) );
+      products.add( new Product( "2222222222222", "Paperclips 2", "Paperclips description 2" ) );
+      products.add( new Product( "3333333333333", "Paperclips 3", "Paperclips description 3" ) );
+      products.add( new Product( "4444444444444", "Paperclips 4", "Paperclips description 4" ) );
+      products.add( new Product( "5555555555555", "Paperclips 5", "Paperclips description 5" ) );
+   }
 
    public Product()
    {
@@ -19,6 +33,48 @@ public class Product
       this.ean = ean;
       this.name = name;
       this.description = description;
+   }
+
+   public static List<Product> findAll()
+   {
+      return new ArrayList<Product>( products );
+   }
+
+   public static Product findByEan( String ean )
+   {
+      for ( Product candidate : products )
+      {
+         if ( candidate.ean.equals( ean ) )
+         {
+            return candidate;
+         }
+      }
+      return null;
+   }
+
+   public static List<Product> findByName( String term )
+   {
+      final List<Product> results = new ArrayList<Product>();
+
+      for ( Product candidate : products )
+      {
+         if ( candidate.name.toLowerCase().contains( term.toLowerCase() ) )
+         {
+            results.add( candidate );
+         }
+      }
+      return results;
+   }
+
+   public static boolean remove( Product product )
+   {
+      return products.remove( product );
+   }
+
+   public void save()
+   {
+      products.remove( findByEan( this.ean ) );
+      products.add( this );
    }
 
    public String toString()
