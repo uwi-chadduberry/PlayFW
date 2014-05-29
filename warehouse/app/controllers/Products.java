ddb36289543404eb45aa526211e514c4c20598bb
Chad Duberry <uwi.chadduberry@gmail.com>
2014-05-28 22:23:49 -0600

    Implemented and modified save(), newProducts() and details() methods

--- warehouse/app/controllers/Products.java
+++ warehouse/app/controllers/Products.java
@@ -15,6 +15,7 @@ import java.util.List;
 public class Products extends Controller
 {
    private static final Form<Product> productForm = Form.form( Product.class );
+
    public static Result list()
    {
       List<Product> products = Product.findAll();
@@ -28,12 +29,28 @@ public class Products extends Controller
 
    public static Result details( String ean )
    {
-      return TODO;
+      final Product product = Product.findByEan( ean );
+      if ( product == null )
+      {
+         return notFound( String.format( "Product %s does not exist.", ean ) );
+      }
+      Form<Product> filledForm = productForm.fill( product );
+      return ok( details.render( filledForm ) );
    }
 
 
    public static Result save()
    {
-      return TODO;
+      Form<Product> boundForm = productForm.bindFromRequest();
+      if ( boundForm.hasErrors() )
+      {
+         flash( "error", "Please correct the form below" );
+         return badRequest( details.render( boundForm ) );
+      }
+
+      Product product = boundForm.get();
+      product.save();
+      flash( "success", String.format( "Successfully added product %s", product ) );
+      return redirect( controllers.routes.Products.list() );
    }
 }
