da406bdfec7ab448a37fbd3fc15bc8d465d060f5
Chad Duberry <uwi.chadduberry@gmail.com>
2014-05-28 19:10:08 -0600

    list() method now works

--- warehouse/app/controllers/Products.java
+++ warehouse/app/controllers/Products.java
@@ -4,14 +4,18 @@ package controllers;
  * Created by CHAD on 5/28/14.
  */
 
+import models.Product;
 import play.mvc.Controller;
 import play.mvc.Result;
+import views.html.products.list;
+import java.util.List;
 
 public class Products extends Controller
 {
    public static Result list()
    {
-      return ok();
+      List<Product> products = Product.findAll();
+      return ok( list.render( products ) );
    }
 
    public static Result newProduct()
