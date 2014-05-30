51967c8f0b74bcdb327c64db77cafac3e99ca88c
Chad Duberry <uwi.chadduberry@gmail.com>
2014-05-30 09:24:56 -0600

    Created contraints for labels

--- todolist/app/models/Task.java
+++ todolist/app/models/Task.java
@@ -1,17 +1,17 @@
 package models;
 
-import java.util.ArrayList;
-import java.util.List;
-
 /**
  * Created by CHAD on 5/30/14.
  */
 
-
+import java.util.ArrayList;
+import java.util.List;
+import play.data.validation.Constraints.*;
 
 public class Task
 {
     public Long id;
+    @Required
     public String label;
     public static List<Task> all()
     {
