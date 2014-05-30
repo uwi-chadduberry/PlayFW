daada1c9f87ce8ad29e0aa8abeccdb4974405f45
Chad Duberry <uwi.chadduberry@gmail.com>
2014-05-30 09:24:09 -0600

    Implemented the task() method

--- todolist/app/controllers/Application.java
+++ todolist/app/controllers/Application.java
@@ -1,20 +1,24 @@
 package controllers;
 
+import models.Task;
 import play.*;
+import play.data.Form;
 import play.mvc.*;
 
 import views.html.*;
 
 public class Application extends Controller {
 
+    static Form<Task> taskForm = Form.form(Task.class);
+
     public static Result index()
     {
-        return redirect( routes.Application.task() );
+        return redirect( controllers.routes.Application.task() );
     }
 
     public static Result task()
     {
-        return TODO;
+        return ok( views.html.index.render( Task.all(), taskForm ) );
     }
 
     public static Result newTask()
@@ -22,7 +26,7 @@ public class Application extends Controller {
         return TODO;
     }
 
-    public static Result deleteTask( Long id)
+    public static Result deleteTask( Long id )
     {
         return TODO;
     }
